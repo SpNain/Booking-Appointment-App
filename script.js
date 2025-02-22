be341d51a8810d03078fa8c108c0ee261f1cc39c
HardeepNain <hnspnain@gmail.com>
2025-02-22 14:31:37 +0530

    Deleting user details from crud-crud using axios

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let url = "https://crudcrud.com/api/b2c7063c92094b77b75d62cd577e0ec9/bookingsData";
-
-// delete aur edit ke time pe dobara se get call lgane ki bjaye mai jb DOMContentLoaded ke time call lgaunga tbhi saare objs store kr lunga taaki baar baar call na lgana pde
-// ab dono ke alag alag faayde h store krne pe baar baar call nhi lgani pdegi but space use hoga
-// baar baar call lgane se space use nhi hoga but data ko baar baar mangwana pdega jise api calls bdegi
+let url =
+  "https://crudcrud.com/api/b2c7063c92094b77b75d62cd577e0ec9/bookingsData";
 let responseData = [];
 
 const nameInput = document.querySelector("#name");
@@ -13,17 +10,17 @@ const dateInput = document.querySelector("#date");
 const bookingForm = document.querySelector("#bookingForm");
 bookingForm.addEventListener("submit", onSubmit);
 
-// DOM load hote hi saare jo jo data pahle se added hoga use get krke ui pe lga dega ye wala code
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get(url)
-        .then((response) => {
-            for (let i = 0; i < response.data.length; i++) {
-                // console.log(response.data[i]);
-                displayUserOnScreen(response.data[i]);
-            }
-        })
-        .catch(error => console.log(error));
-})
+  axios
+    .get(url)
+    .then((response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        // console.log(response.data[i]);
+        displayUserOnScreen(response.data[i]);
+      }
+    })
+    .catch((error) => console.log(error));
+});
 
 function onSubmit(event) {
   event.preventDefault();
@@ -37,21 +34,18 @@ function onSubmit(event) {
 }
 
 function addDetailsCard(event) {
-
   let userDetailsObj = {
     userName: `${nameInput.value}`,
     userEmail: `${emailInput.value}`,
     userPhone: `${phoneInput.value}`,
-    userDate: `${dateInput.value}`
+    userDate: `${dateInput.value}`,
   };
 
   if (userDetailsObj.userName == "" || userDetailsObj.userPhone == "") {
     alert(`Fill all the details`);
-  }
-  else if (isEmailExist(userDetailsObj)) { // check if mail already exists or not
+  } else if (isEmailExist(userDetailsObj)) {
     alert(`${userDetailsObj.userEmail} already exists. Try another Email.`);
-  }
-  else {
+  } else {
     axios
       .post(url, userDetailsObj)
       .then((response) => displayUserOnScreen(response.data))
@@ -60,8 +54,9 @@ function addDetailsCard(event) {
 }
 
 function displayUserOnScreen(userDetailsObj) {
-  document.querySelector(".detailsObjectsDiv").innerHTML +=
-        `<div class="card"  id=${userDetailsObj.userEmail}>
+  document.querySelector(
+    ".detailsObjectsDiv"
+  ).innerHTML += `<div class="card"  id=${userDetailsObj.userEmail}>
             <div>
                 <h5> ${userDetailsObj.userName} </h5>
                 <h6> Email : ${userDetailsObj.userEmail} </h6>
@@ -73,26 +68,49 @@ function displayUserOnScreen(userDetailsObj) {
                 <button class="btn btn-primary m-2 p-2" onclick="editDetailsCard(event)">Edit</button>
             </div>
         </div>`;
-    
-    updateResponseData(userDetailsObj); // jaise hi new object add hoga ya pahle se added objects load honge to responsedata ko update kr denge
+
+  updateResponseData(userDetailsObj);
 }
 
-// response data ko udpate krne ke kaam h iska
-function updateResponseData(userDetailsObj) {
+function updateResponseData(userDetailsObj, targetId) {
+  if (userDetailsObj) {
     responseData.push(userDetailsObj);
     console.log(responseData);
+  } else {
+    responseData = responseData.filter(
+      (userDetailsObj) => userDetailsObj._id != targetId
+    );
+    console.log(responseData);
+  }
 }
 
-// email dobara na aaye isiliye ye fxn bnaaya h ye check krne ke liye ki mail exist krti h ki nhi
 function isEmailExist(userDetailsObj) {
-    for (let i = 0; i < responseData.length; i++){
-        if (responseData[i].userEmail == userDetailsObj.userEmail) return true;
-    }
-    return false;
+  for (let i = 0; i < responseData.length; i++) {
+    if (responseData[i].userEmail == userDetailsObj.userEmail) return true;
+  }
+  return false;
 }
 
 function deleteDetailsCard(event) {
-  localStorage.removeItem(event.target.parentNode.parentNode.id);
+  // from crudcrud
+  let targetId = "";
+
+  for (let i = 0; i < responseData.length; i++) {
+    if (responseData[i].userEmail == event.target.parentNode.parentNode.id) {
+      targetId = responseData[i]._id;
+      console.log(targetId);
+    }
+  }
+
+  axios
+    .delete(`${url}/${targetId}`)
+    .then((response) => {
+      console.log(response);
+      updateResponseData(null, targetId); // responseData me se jo object abhi delete kra h usko nikal denge
+    })
+    .catch((error) => console.log(error));
+
+  // from UI
   event.target.parentNode.parentNode.remove();
 }
 
