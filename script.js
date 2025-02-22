ec06d0d410d6e73a577d3ad553d83a26be272df5
HardeepNain <hnspnain@gmail.com>
2025-02-22 14:35:01 +0530

    Code converted into async-await

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ const dateInput = document.querySelector("#date");
 const bookingForm = document.querySelector("#bookingForm");
 bookingForm.addEventListener("submit", onSubmit);
 
-window.addEventListener("DOMContentLoaded", () => {
-  axios
-    .get(url) // crud crud se data get kra
-    .then((response) => {
-      for (let i = 0; i < response.data.length; i++) {
-        // console.log(response.data[i]);
-        updateResponseData("getORpost", response.data[i]); // data update krdo
-        displayUserOnScreen(response.data[i]); // ui pe update krdo
-      }
-    })
-    .catch((error) => console.log(error));
-});
+window.addEventListener("DOMContentLoaded", refresh);
+
+async function refresh() {
+  try {
+    let response = await axios.get(url);
+    for (let i = 0; i < response.data.length; i++) {
+      // console.log(response.data[i]);
+      updateResponseData("getORpost", response.data[i]);
+      displayUserOnScreen(response.data[i]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function onSubmit(event) {
   event.preventDefault();
@@ -34,51 +36,49 @@ function onSubmit(event) {
   dateInput.value = "";
 }
 
-function addDetailsCard(event) {
+async function addDetailsCard(event) {
   let userDetailsObj = {
     userName: `${nameInput.value}`,
     userEmail: `${emailInput.value}`,
     userPhone: `${phoneInput.value}`,
     userDate: `${dateInput.value}`,
-    uniqueKey: `${new Date().getTime()}`, // creating a new key value pair so that in all similar type of project like expense tracker & seller admin panel there is a similarity in syntax throughtout the code
+    uniqueKey: `${new Date().getTime()}`,
   };
 
-  // as i removed the userEmail from uniqueKey so there is not point to keep checking that same email exists or not
-  // i mean i can check but then i have to adjust all the future code accordingly which will take time so i think its best to just remove that functionality
-  if (userDetailsObj.userName == "" || userDetailsObj.userPhone == "" || userDetailsObj.userEmail == "") {
+  if (
+    userDetailsObj.userName == "" ||
+    userDetailsObj.userPhone == "" ||
+    userDetailsObj.userEmail == ""
+  ) {
     alert(`Fill all the details`);
   } else {
-    axios
-      .post(url, userDetailsObj) // crud crud pe add krdo
-      .then((response) => {
-        // console.log(response.data);
-        updateResponseData("getORpost", response.data); // #1 - data update krdo
-        displayUserOnScreen(response.data); // ui pe add krdo
-      })
-      .catch((error) => console.log(error));
+    try {
+      let response = await axios.post(url, userDetailsObj);
+      // console.log(response.data);
+      updateResponseData("getORpost", response.data);
+      displayUserOnScreen(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
-function deleteDetailsCard(event) {
-
+async function deleteDetailsCard(event) {
   let targetObj = getTargetObj(event.target.parentNode.parentNode);
   // console.log(targetObj._id);
 
-  axios
-    .delete(`${url}/${targetObj._id}`) // crud crud se delete krdo
-    .then((response) => {
-      // console.log(response.data);
-      updateResponseData("delete", targetObj); // data update krdo
-      event.target.parentNode.parentNode.remove(); // ui se delete krdo
-    })
-    .catch((error) => console.log(error));
+  try {
+    let response = await axios.delete(`${url}/${targetObj._id}`);
+    updateResponseData("delete", targetObj);
+    event.target.parentNode.parentNode.remove();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function editDetailsCard(event) {
   let targetObj = getTargetObj(event.target.parentNode.parentNode);
 
-
-  // rather than refilling the details in main form we create another form within card. which looks same as of main form but with just updated edit word everywhere
   document.getElementById(
     `${targetObj.uniqueKey}`
   ).innerHTML = `<form action="#" class="w-75 mx-auto" id="editBookingForm">
@@ -114,38 +114,29 @@ function editDetailsCard(event) {
     </div>
     </form>`;
 
-  // after filling details when we submit the form cb fxn will run due to following code
   const editBookingForm = document.querySelector("#editBookingForm");
   editBookingForm.addEventListener("submit", addEditedCard);
 }
 
-function addEditedCard(event) {
-
+async function addEditedCard(event) {
   event.preventDefault();
 
-  // get object jisko update krna h
   let targetObj = getTargetObj(event.target.parentNode);
 
-  // updated object banao
   let editedUserObj = {
     userName: event.target.editName.value,
     userEmail: event.target.editEmail.value,
     userPhone: event.target.editPhone.value,
     userDate: event.target.editDate.value,
     uniqueKey: targetObj.uniqueKey,
-    // hmari uniqueKey to hmesha same hi rhegi kisi ek particular obj ke liye
-    // but kyunki put ke time pe hume jo chije update nhi bhi krni hoti unhe bhi same to same bhejna pdta h to isiliye iss obj me uniqueKey ko add krna pdega hume
   };
 
-    axios
-      .put(`${url}/${targetObj._id}`, editedUserObj) // crud crud pe update krdo
-      .then((response) => {
-        // console.log(response.data);
-        updateResponseData("put", editedUserObj); // data update krdo
+  try {
+    let response = await axios.put(`${url}/${targetObj._id}`, editedUserObj);
+    // console.log(response.data);
+    updateResponseData("put", editedUserObj);
 
-        // ui pe update krdo
-    document.getElementById(`${targetObj.uniqueKey}`).innerHTML =
-    `<div>
+    document.getElementById(`${targetObj.uniqueKey}`).innerHTML = `<div>
       <h5> ${editedUserObj.userName} </h5>
       <h6> Email : ${editedUserObj.userEmail} </h6>
       <h6> Phone Number : ${editedUserObj.userPhone} </h6>
@@ -155,8 +146,9 @@ function addEditedCard(event) {
       <button class="btn btn-danger m-2 p-2" onclick="deleteDetailsCard(event)">X</button>
       <button class="btn btn-primary m-2 p-2" onclick="editDetailsCard(event)">Edit</button>
     </div>`;
-      })
-      .catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayUserOnScreen(userDetailsObj) {
@@ -173,30 +165,23 @@ function displayUserOnScreen(userDetailsObj) {
   <button class="btn btn-danger m-2 p-2" onclick="deleteDetailsCard(event)">X</button>
   <button class="btn btn-primary m-2 p-2" onclick="editDetailsCard(event)">Edit</button>
   </div>
-        </div>`;
+  </div>`;
 }
 
 function updateResponseData(requestType, paramObj) {
   if (requestType == "getORpost") {
-    // nya user add hua h use add kdo
     responseData.push(paramObj);
     // console.log(responseData);
-  }
-  else if (requestType == "delete") {
-    // user delete hua h to use nikal to array me se
+  } else if (requestType == "delete") {
     responseData = responseData.filter((ele) => ele._id != paramObj._id);
     // console.log(responseData);
-  }
-  else if (requestType == "put") {
-    // user ko update kra h to use dund ke(using uniqueKey because _id didn't exist in paramObj in case of put request) uski details ko update krdo
+  } else if (requestType == "put") {
     for (let i = 0; i < responseData.length; i++) {
       if (responseData[i].uniqueKey == paramObj.uniqueKey) {
-        // this is our target object which we need to update
         responseData[i].userName = paramObj.userName;
         responseData[i].userEmail = paramObj.userEmail;
         responseData[i].userPhone = paramObj.userPhone;
         responseData[i].userDate = paramObj.userDate;
-        // unique key to same hi h to use update krne ki jrurat nhi h yhape
         // console.log(responseData);
         break;
       }
@@ -209,24 +194,3 @@ function getTargetObj(element) {
     if (responseData[i].uniqueKey == element.id) return responseData[i];
   }
 }
-
-/*
-#1.
-jb bhi hum axios se crud crud pe 
-get ki request lgate h to hume response.data me array milta h jisme objs hote h
-post ki request lgate h to response.data me jo object post kra h wo milta h with addtion key:value pair jo crud crud genrate krta h _id ke naam se
-delete aur put ki request pe response milta h but respone.data me empty string hoti h matlab kuch nhi milta
-
-Isiliye jb mai post ke time responseData me khud se userDetailsObj push krta hu to usme _id wali key nhi hoti h to jisse hmare delete aur edit button kaam nhi krte kyunki inme humne _id ki jrurat pdti h
-but agr maine page refresh kr diya to fir get ki request lgti h dom load hone pe aur jisse responseData array me objects dobara se push hote h aur wo objects aate h response.data me
-ab isse hota ye h ki ab saare objects ke pass _id key ho jaati h aur sbpe delete aur edit button kaam krne lg jaate h
-
-to ab mere pass do option the yato mai jb post kru to jo fxn dom load hone pe chal rha h use call kr deta
-ya fir userDetails obj ki bjaye response.data(joki userDetailsObj hi h with _id) ko pass kr deta
-so i went with latter one.
-*/
-
-/*
-So basically get ke time pe hume data sirf ui pe update krna hota h aur post,delete and put ke case me data hume ui pe aur backend pe dono jgah update krna hota h joki hum har ek request pe kr bhi rhe h
-Aur kyunki hum data ko yahpe store kr rhe h baar baar use krne ke liye to hume har ek call pe use update krna pdega
-*/
