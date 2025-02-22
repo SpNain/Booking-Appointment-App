b5253a06dae38d6c4dda93df438205c05b8300bb
HardeepNain <hnspnain@gmail.com>
2025-02-22 14:31:05 +0530

    Fetching user details from crud-crud using axios

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-// Yhape mai iss app ko nya look de rha hu bas aur kuch nhi aur bas thode bhut naam change kre h fxns objects ke baaki logic to same hi h
+let url = "https://crudcrud.com/api/b2c7063c92094b77b75d62cd577e0ec9/bookingsData";
 
-// Aur isme hum sirf user ko local storage se htake crud crud pe save krne ka code change kr rhe h logic wise
-// aur ui pe save krne wala code ka logic same h but thoda sa code me change h
+// delete aur edit ke time pe dobara se get call lgane ki bjaye mai jb DOMContentLoaded ke time call lgaunga tbhi saare objs store kr lunga taaki baar baar call na lgana pde
+// ab dono ke alag alag faayde h store krne pe baar baar call nhi lgani pdegi but space use hoga
+// baar baar call lgane se space use nhi hoga but data ko baar baar mangwana pdega jise api calls bdegi
+let responseData = [];
 
-let url =
-  "https://crudcrud.com/api/b2c7063c92094b77b75d62cd577e0ec9/bookingsData";
-
-// get all inputs - agr mai inko naa bhi nikalu to bhi sidha nameInput ki jgaha name use krunga to muje name ke input ki value mil jaayegi due to label
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const phoneInput = document.querySelector("#phone");
 const dateInput = document.querySelector("#date");
 
-// on form submission onSubmit fxn will run
 const bookingForm = document.querySelector("#bookingForm");
 bookingForm.addEventListener("submit", onSubmit);
+
+// DOM load hote hi saare jo jo data pahle se added hoga use get krke ui pe lga dega ye wala code
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get(url)
+        .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+                // console.log(response.data[i]);
+                displayUserOnScreen(response.data[i]);
+            }
+        })
+        .catch(error => console.log(error));
+})
 
 function onSubmit(event) {
   event.preventDefault();
@@ -28,19 +37,21 @@ function onSubmit(event) {
 }
 
 function addDetailsCard(event) {
+
   let userDetailsObj = {
     userName: `${nameInput.value}`,
     userEmail: `${emailInput.value}`,
-    userPhone: `${phoneInput.value}`, // have to rename form "Phone Number" to "userPhone" because apis isn't working with previous one
+    userPhone: `${phoneInput.value}`,
     userDate: `${dateInput.value}`
   };
 
   if (userDetailsObj.userName == "" || userDetailsObj.userPhone == "") {
     alert(`Fill all the details`);
-  } else if (localStorage.getItem(userDetailsObj.userEmail)) {
-    alert(`${userDetailsObj.Email} already exists. Try another Email.`);
-  } else {
-    // instead of local storage now we add detailsObj in crud-crud database using axios
+  }
+  else if (isEmailExist(userDetailsObj)) { // check if mail already exists or not
+    alert(`${userDetailsObj.userEmail} already exists. Try another Email.`);
+  }
+  else {
     axios
       .post(url, userDetailsObj)
       .then((response) => displayUserOnScreen(response.data))
@@ -62,6 +73,22 @@ function displayUserOnScreen(userDetailsObj) {
                 <button class="btn btn-primary m-2 p-2" onclick="editDetailsCard(event)">Edit</button>
             </div>
         </div>`;
+    
+    updateResponseData(userDetailsObj); // jaise hi new object add hoga ya pahle se added objects load honge to responsedata ko update kr denge
+}
+
+// response data ko udpate krne ke kaam h iska
+function updateResponseData(userDetailsObj) {
+    responseData.push(userDetailsObj);
+    console.log(responseData);
+}
+
+// email dobara na aaye isiliye ye fxn bnaaya h ye check krne ke liye ki mail exist krti h ki nhi
+function isEmailExist(userDetailsObj) {
+    for (let i = 0; i < responseData.length; i++){
+        if (responseData[i].userEmail == userDetailsObj.userEmail) return true;
+    }
+    return false;
 }
 
 function deleteDetailsCard(event) {
