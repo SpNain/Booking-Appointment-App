376169ee0f1d98fd95240d7e0d3a58946132353c
HardeepNain <hnspnain@gmail.com>
2025-02-22 14:29:37 +0530

    Saving User details on crud-crud using axios

--- script.js
+++ script.js
@@ -1,73 +1,83 @@
-// Task Solution
-function showDetails(event) {
-    event.preventDefault();
+// Yhape mai iss app ko nya look de rha hu bas aur kuch nhi aur bas thode bhut naam change kre h fxns objects ke baaki logic to same hi h
 
-    addCard(event);
+// Aur isme hum sirf user ko local storage se htake crud crud pe save krne ka code change kr rhe h logic wise
+// aur ui pe save krne wala code ka logic same h but thoda sa code me change h
 
-    // empty input fields
-    event.target.name.value = "";
-    event.target.email.value = "";
-    event.target.phone.value = "";
-    event.target.date.value = "";
+let url =
+  "https://crudcrud.com/api/b2c7063c92094b77b75d62cd577e0ec9/bookingsData";
 
+// get all inputs - agr mai inko naa bhi nikalu to bhi sidha nameInput ki jgaha name use krunga to muje name ke input ki value mil jaayegi due to label
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const dateInput = document.querySelector("#date");
+
+// on form submission onSubmit fxn will run
+const bookingForm = document.querySelector("#bookingForm");
+bookingForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  addDetailsCard(event);
+
+  nameInput.value = "";
+  emailInput.value = "";
+  phoneInput.value = "";
+  dateInput.value = "";
 }
 
-function addCard(event){
+function addDetailsCard(event) {
+  let userDetailsObj = {
+    userName: `${nameInput.value}`,
+    userEmail: `${emailInput.value}`,
+    userPhone: `${phoneInput.value}`, // have to rename form "Phone Number" to "userPhone" because apis isn't working with previous one
+    userDate: `${dateInput.value}`
+  };
 
-    // in local storage
-    let detailObj = {
-        "Name" : event.target.name.value,
-        "Email" : event.target.email.value,
-        "Phone Number" : event.target.phone.value,
-        "Date" : event.target.date.value
-    };
+  if (userDetailsObj.userName == "" || userDetailsObj.userPhone == "") {
+    alert(`Fill all the details`);
+  } else if (localStorage.getItem(userDetailsObj.userEmail)) {
+    alert(`${userDetailsObj.Email} already exists. Try another Email.`);
+  } else {
+    // instead of local storage now we add detailsObj in crud-crud database using axios
+    axios
+      .post(url, userDetailsObj)
+      .then((response) => displayUserOnScreen(response.data))
+      .catch((error) => console.log(error));
+  }
+}
 
-    let uniqueKey = detailObj.Email;
-    if(detailObj.Name == '' || detailObj["Phone Number"] == ''){ // emails is already required && date doesn't included
-        alert(`Fill all the details`);
-    }
-    else if(localStorage.getItem(detailObj.Email)){
-        alert(`${detailObj.Email} already exists. Try another Email.`);
-    }
-    else{
-        localStorage.setItem(uniqueKey, JSON.stringify(detailObj));
-
-        // on UI
-        document.querySelector(".detailDiv").innerHTML += 
-        `<div class="card"  id=${detailObj.Email}>
-            <div class="card-body">
-                <h5 class="card-title">${detailObj.Name}</h5>
-                <p class="card-text">
-                    <h6> Email : ${detailObj.Email} </h6>
-                    <h6> Phone Number : ${detailObj['Phone Number']} </h6>
-                    <button class="btn btn-danger btn-sm delete float-right" onclick="deleteCard(event)">X</button>
-                    <button class="btn btn-primary btn-sm mx-2 float-right" onclick="editCard(event)">Edit</button>
-                </p>
+function displayUserOnScreen(userDetailsObj) {
+  document.querySelector(".detailsObjectsDiv").innerHTML +=
+        `<div class="card"  id=${userDetailsObj.userEmail}>
+            <div>
+                <h5> ${userDetailsObj.userName} </h5>
+                <h6> Email : ${userDetailsObj.userEmail} </h6>
+                <h6> Phone Number : ${userDetailsObj.userPhone} </h6>
+                <h6> Date : ${userDetailsObj.userDate} </h6>
             </div>
-        </div>`
-    }
+            <div class ="d-flex justify-content-end">
+                <button class="btn btn-danger m-2 p-2" onclick="deleteDetailsCard(event)">X</button>
+                <button class="btn btn-primary m-2 p-2" onclick="editDetailsCard(event)">Edit</button>
+            </div>
+        </div>`;
 }
 
-function deleteCard(event){
-
-    // from local storage
-    localStorage.removeItem(event.target.parentNode.parentNode.id);
-
-    // from UI
-    event.target.parentNode.parentNode.remove();
+function deleteDetailsCard(event) {
+  localStorage.removeItem(event.target.parentNode.parentNode.id);
+  event.target.parentNode.parentNode.remove();
 }
 
-function editCard(event){
+function editDetailsCard(event) {
+  let userDetailsObj = JSON.parse(
+    localStorage.getItem(event.target.parentNode.parentNode.id)
+  );
 
-    // get object from local storage
-    let detailObj = JSON.parse(localStorage.getItem(event.target.parentNode.parentNode.id));
+  nameInput.value = userDetailsObj.userName;
+  emailInput.value = userDetailsObj.userEmail;
+  phoneInput.value = userDetailsObj.userPhone;
+  dateInput.value = userDetailsObj.userDate;
 
-    // set details into input fields
-    document.querySelector('input[name="name"]').value = detailObj.Name;
-    document.querySelector('input[name="email"]').value = detailObj.Email;
-    document.querySelector('input[name="phone"]').value = detailObj["Phone Number"];
-    document.querySelector('input[name="date"]').value = detailObj.Date;
-
-    // delete from UI & local Storage
-    deleteCard(event);
+  deleteDetailsCard(event);
 }
